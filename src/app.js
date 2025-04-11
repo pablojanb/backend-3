a82a8385ce22b007fcc3657dc9d5a36f5770b401
pabloj <pablojanb@gmail.com>
2025-04-11 03:21:49 -0300

    agrego documentacion con swagger

--- src/app.js
+++ src/app.js
@@ -11,6 +11,8 @@ import sessionsRouter from './routes/sessions.router.js';
 import { handleErrors } from './middleware/handleErrors.js';
 import { addLogger } from './config/logger.js';
 import { config } from './config/config.js';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express'
 
 const app = express();
 const PORT = config.PORT||8080;
@@ -26,6 +28,36 @@ app.use('/api/adoptions',adoptionsRouter);
 app.use('/api/sessions',sessionsRouter);
 app.use('/api/mocks', mockingRouter);
 app.use('/api/loggerTest', testsRouter);
+
+const swaggerOpts = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Api Adopt Me",
+            version: "1.0.0",
+            description: "APIrest para applicación de adopción de mascotas",
+        },
+        servers: [
+            {
+                url: 'http://localhost:8080',
+                description: 'Produccion'
+            },
+            {
+                url: 'http://localhost:9090',
+                description: 'Desarrollo'
+            },
+            {
+                url: 'http://localhost:8081',
+                description: 'Testing'
+            }
+        ]
+    },
+    apis: ["./src/docs/*.yaml"]
+}
+
+const spec = swaggerJSDoc(swaggerOpts)
+
+app.use('/swagger', swaggerUi.serve,swaggerUi.setup(spec))
 
 app.use(handleErrors)
 
